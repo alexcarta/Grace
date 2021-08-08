b6696feca42bb1ae0290bc87447e798ff8773009
Alessandro Carta <alessandro.carta91@gmail.com>
2021-08-08 09:18:29 +0200

    scrolltop done

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,14 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 
+// SHOW SCROLL TOP
+
+function scrollTop(){
+  const scrollTop = document.getElementById('scroll-top')
+  if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
 
 
 
